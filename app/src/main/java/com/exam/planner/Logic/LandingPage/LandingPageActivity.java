7b36d32e985851a14bf17cedf2c23564f85883dd
Kyle <lamour13@myumanitoba.ca>
2019-06-05 02:02:09 -0500

    Base setup

---
+++ app/src/main/java/com/exam/planner/Logic/LandingPage/LandingPageActivity.java
@@ -0,0 +1,15 @@
+package com.exam.planner.Logic.LandingPage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.exam.planner.R;
+
+public class LandingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
