70d72f7bc4a684ee851a0b143c7ab4640d053123
suiq <suiq@myumanitoba.ca>
2019-06-06 22:56:41 -0500

    Database setup

--- app/src/main/java/com/exam/planner/Logic/Dao/User.java
+++ app/src/main/java/com/exam/planner/Logic/Dao/User.java
@@ -1,4 +1,24 @@
 package com.exam.planner.Logic.Dao;
 
 public class User {
+    String username;
+    String password;
+    String schedule;
+
+    public User(){
+        this.username = null;
+        this.password = null;
+        this.schedule = null;
+    }
+
+    public User(String un, String pwd, String sch){
+        this.username = un;
+        this.password = pwd;
+        this.schedule = sch;
+    }
+
+    public String toString(){
+        return "UserName: "+username + "\tPassword: "+password+"\tSchedule: "+schedule;
+    }
+
 }
